f3d0f8a4b165fac3b0bc8a69577cdda7690349f8
rplaganas <rplaganas@192.168.56.1>
2016-09-15 08:20:40 +0800

    Adds first regex

--- Regexmatch/src/regexmatch/Regexmatch.java
+++ Regexmatch/src/regexmatch/Regexmatch.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package regexmatch;
 
-/**
- *
- * @author rplaganas
- */
+
 public class Regexmatch {
 
-    /**
-     * @param args the command line arguments
-     */
+public static boolean isTrue(String str){
+    return str.matches("true");
+}
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(isTrue("false"));
     }
     
 }
